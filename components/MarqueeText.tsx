3b85b5ab168ab3bb9c5773ab85739dd1a4c3e06d
Marcos Lopez <juanysacreu@hotmail.com>
2026-02-09 11:43:43 +0100

    UI: fix MarqueeText animation and character truncation issues for driver names

--- components/MarqueeText.tsx
+++ components/MarqueeText.tsx
@@ -34,7 +34,6 @@ export const MarqueeText: React.FC<MarqueeTextProps> = ({ text, className = '' }
                 ref={textRef}
                 className={`
                     ${className} 
-                    block 
                     whitespace-nowrap 
                     ${shouldAnimate ? 'animate-marquee' : 'truncate'}
                     transition-all
